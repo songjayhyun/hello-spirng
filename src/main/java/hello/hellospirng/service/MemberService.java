1c960473a5e983164084e8925b15d64390422eab
songjayhyun <thewinner1st@naver.com>
2021-07-01 15:47:12 +0900

    service&test

---
+++ src/main/java/hello/hellospirng/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospirng.service;
+
+import hello.hellospirng.domain.Member;
+import hello.hellospirng.repository.MemberRepository;
+import hello.hellospirng.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+
+        // 중복 이름은 안된다는 임의 규정
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+
+}
--- src/test/java/hello/hellospirng/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospirng/repository/MemoryMemberRepositoryTest.java
@@ -1,24 +1,18 @@
 package hello.hellospirng.repository;
 
 import hello.hellospirng.domain.Member;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
-    MemoryMemberRepository repository= new MemoryMemberRepository();
-
-    @AfterEach
-    public void afterEach() {
-        repository.clearStore();
-    }
-
+    MemberRepository repository= new MemoryMemberRepository();
 
     @Test
     public void save() {
@@ -29,9 +23,7 @@ class MemoryMemberRepositoryTest {
         repository.save(member);
 
         Member result = repository.findById(member.getId()).get();
-        System.out.println("result = " + (result==member));
-        assertEquals(member,result);
-        assertThat(member).isEqualTo(result);
+        assertThat(result).isEqualTo(member);
     }
 
     @Test
@@ -46,23 +38,27 @@ class MemoryMemberRepositoryTest {
         repository.save(member2);
 
         Member result = repository.findByName("spring1").get();
-        assertThat(result).isEqualTo(member1);
+
+        assertThat(result).isEqualTo(result);
+
     }
 
     @Test
     public void findAll() {
 
+        //given
         Member member1 = new Member();
-        member1.setName("spring1");
+        member1.setName("spring");
         repository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring2");
         repository.save(member2);
 
+        //when
         List<Member> result = repository.findAll();
 
+        //then
         assertThat(result.size()).isEqualTo(2);
     }
-
 }
