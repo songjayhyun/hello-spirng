8a6d939752e41403453db454fdbf205223422963
songjayhyun <thewinner1st@naver.com>
2021-07-03 11:24:28 +0900

    jdbc & integration test

--- src/main/java/hello/hellospirng/service/SpringConfig.java
+++ src/main/java/hello/hellospirng/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hellospirng.service;
 
+import hello.hellospirng.repository.JdbcMemberRepository;
+import hello.hellospirng.repository.JdbcTemplateMemberRepository;
 import hello.hellospirng.repository.MemberRepository;
-import hello.hellospirng.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
