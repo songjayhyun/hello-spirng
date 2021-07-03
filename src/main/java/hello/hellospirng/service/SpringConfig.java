9def7f8706b26c99ca16c97f9f9a1aed0eb7ccb6
songjayhyun <thewinner1st@naver.com>
2021-07-03 21:42:48 +0900

    JPA & Spring data jpa & AOP

--- src/main/java/hello/hellospirng/service/SpringConfig.java
+++ src/main/java/hello/hellospirng/service/SpringConfig.java
@@ -1,33 +1,48 @@
 package hello.hellospirng.service;
 
+import hello.hellospirng.aop.TimeTraceApp;
 import hello.hellospirng.repository.JdbcMemberRepository;
 import hello.hellospirng.repository.JdbcTemplateMemberRepository;
+import hello.hellospirng.repository.JpaMemberRepository;
 import hello.hellospirng.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private EntityManager em;
     private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
         this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+
+//    @Bean
+//    public TimeTraceApp timeTraceApp() {
+//        return new TimeTraceApp();
+//    }
+
+/*
     @Bean
     public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
+        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
         return new JdbcTemplateMemberRepository(dataSource);
-    }
+        return new JpaMemberRepository(em);
+    };
+*/
 }
